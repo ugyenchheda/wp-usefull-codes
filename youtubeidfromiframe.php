565fb6dea64bf92f8d71239c88fe985854547639
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-08-07 22:57:49 +0300

    Update youtubeidfromiframe.php

--- youtubeidfromiframe.php
+++ youtubeidfromiframe.php
@@ -1,4 +1,4 @@
-//function to get youtube id from youtube url
+<?php //function to get youtube id from youtube url
 	function get_youtube_video_id($url) {
 		$query_string = parse_url($url, PHP_URL_QUERY);
 		parse_str($query_string, $query_params);
@@ -14,3 +14,5 @@
 $video_link = get_post_meta( get_the_ID(), 'video_link', true );
 $get_video_id = get_youtube_video_id($video_link);
 $final_video = 'https://www.youtube.com/watch?v='. $get_video_id;
+
+?>
